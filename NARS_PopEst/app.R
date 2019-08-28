e9a2046511ccb893934c33943ca97706c13ba423
Karen Blocksom <blocksom.karen@epa.gov>
2019-08-28 07:49:22 -0700

    Created separate global.r file, added variable selection features and outputs to show only selected variables once they are selected and a button is clicked to subset data.Added checkbox to change coordinates from lat/long to Albers projection (UTM) coordinates.

--- NARS_PopEst/app.R
+++ NARS_PopEst/app.R
@@ -7,8 +7,7 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(spsurvey)
+source("global.r")
 
 
 # Define UI for application that draws a histogram
@@ -18,44 +17,59 @@ ui <- fluidPage(
    titlePanel("NARS Population Estimate Calculation Tool"),
    
    # Sidebar panel for inputs
-   sidebarLayout(
-      sidebarPanel(
+   
+      fluidRow(
         # Input: Select a file ---
-        fileInput(inputId='file1', buttonLabel='Browse...', 
+        column(3,
+            fileInput(inputId='file1', buttonLabel='Browse...', 
                   label='Select CSV file for analysis',
                   multiple=FALSE, accept=c('text/csv','text/comma-separated-values,text/plain','.csv')
                   ),
-      # Horizontal line ----
-      tags$hr(),
-      # Input: checkbox if file has header, default to TRUE ----
-      checkboxInput('header','Header',TRUE),
-      # Input: Select delimiter ----
-      radioButtons("sep","Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
+            # Horizontal line ----
+            tags$hr(),
+            # Input: checkbox if file has header, default to TRUE ----
+            checkboxInput('header','Header',TRUE),
+            # Input: Select delimiter ----
+            radioButtons("sep","Separator",
+                         choices = c(Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"),
+                         selected = ","),
+            
+            # Horizontal line
+            tags$hr(),
+            
+            # Input: Select number of rows to display ----
+            radioButtons("disp","Display",
+                         choices = c(Head = 'head',
+                                     All = 'all'),
+                         selected='head')),
+        
+      column(4,
+        selectizeInput("siteVar","Select site variable", choices=NULL, multiple=FALSE),
+        selectizeInput("coordxVar","Select the Albers X coordinate variable", choices=NULL, multiple=FALSE),
+        selectizeInput("coordyVar","Select the Albers Y coordinate variable", choices=NULL, multiple=FALSE),
+        selectizeInput("weightVar","Select weight variable", choices=NULL, multiple=FALSE),
+        selectizeInput("respVar","Select up to 5 response variables", choices=NULL, multiple=TRUE),
+        selectizeInput("subpopVar","Select up to 5 subpopulation variables", choices=NULL, multiple=TRUE),
+        checkboxInput('natpop','Include national estimates?',TRUE)
+      ),
       
-      # Horizontal line
-      tags$hr(),
+      column(2,
+             radioButtons("xy", "Convert latitude/longitude \nto Albers Projection",
+                          choices = c(Yes = 'latlong', No = 'utm'),
+                          selected = 'utm')
+      )
       
-      # Input: Select number of rows to display ----
-      radioButtons("disp","Display",
-                   choices = c(Head = 'head',
-                               All = 'all'),
-                   selected='head'),
-      
-      selectizeInput("siteVar","Site variable", choices=NULL, multiple=FALSE),
-      selectizeInput("weightVar","Weight variable", choices=NULL, multiple=FALSE),
-      selectizeInput("respVar","Response variable(s)", choices=NULL, multiple=TRUE),
-      selectizeInput("subpopVar","Subpopulation variable(s)", choices=NULL, multiple=TRUE)
       
    ),
-      # Show a plot of the generated distribution
-      mainPanel(
-         tableOutput("contents")
-      )
-    )
+   
+      actionButton("subsetBtn", "Show selected data"),
+   
+      # Show a table of the data
+      tableOutput("contents")
+      
+    
   )
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
@@ -72,6 +86,8 @@ server <- function(input, output, session) {
     updateSelectizeInput(session, "weightVar", "Select weight variable", choices=vars)
     updateSelectizeInput(session, 'respVar', 'Select up to 5 response variables', choices=vars, selected = NULL, 
                          options = list(maxItems=5))
+    updateSelectizeInput(session, 'coordxVar', 'Select the Albers X coordinate variable', choices=vars, selected = NULL)
+    updateSelectizeInput(session, 'coordyVar', 'Select the Albers Y coordinate variable', choices=vars, selected = NULL)
     updateSelectizeInput(session, 'siteVar', 'Select site variable', choices=vars)
     updateSelectizeInput(session, 'subpopVar', 'Select up to 5 subpopulation variables', choices=vars, selected=NULL,
                          options = list(maxItems=5))
@@ -79,16 +95,49 @@ server <- function(input, output, session) {
     df
   })
   
+  # dataOut <- eventReactive(input$subsetBtn,{
+  #   if(input$subsetBtn > 0){
+  #     df1 <- subset(dataIn(), select=c(input$siteVar, input$coordxVar, input$coordyVar, input$weightVar, input$respVar, input$subpopVar))
+  #   }else{
+  #     df1 <- dataIn()
+  #   }
+  # }
+  #   
+  # )
+  # 
+  # output$contents <- renderTable({
+  # 
+  #    # if(input$disp == 'head'){
+  #    #   return(head(dataIn()))
+  #    # } else{
+  #    #   return(dataIn())
+  #    # }
+  #   return(head(dataIn()))
+  #  })
+  # 
+  # output$dataOut <- renderTable({
+  #   dataIn() %>%
+  #     subset(select=input$siteVar, input$coordxVar, input$coordyVar, input$weightVar, input$respVar, input$subpopVar) %>%
+  #     head 
+  # })
   
   output$contents <- renderTable({
-  
-     if(input$disp == 'head'){
-       return(head(dataIn()))
-     } else{
-       return(dataIn())
-     }
-      
-   })
+    if(input$subsetBtn > 0){
+      dataIn() %>%
+        subset(select=c(input$siteVar, input$coordxVar, input$coordyVar, input$weightVar, input$respVar, input$subpopVar)) %>%
+        head 
+    }else{
+      if(input$disp == 'head'){
+        return(head(dataIn()))
+      }else{
+        return(dataIn())
+      }
+    }
+  })
+
+  session$onSessionEnded(function() {
+    stopApp()
+  })  
 }
 
 # Run the application 
