a31f869ae7c2684127013522392e41dd5f0aadaa
Karen Blocksom <blocksom.karen@epa.gov>
2022-10-25 10:32:40 -0700

    Added google tag manager code to allow Google Analytics to work.

--- app.R
+++ app.R
@@ -13,6 +13,16 @@ source("global.r")
 ui <- fluidPage(
   tags$html(class = "no-js", lang="en"),
   tags$head(
+    HTML(
+      "<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-L8ZB');</script>
+		<!-- End Google Tag Manager -->
+		"
+    ),
     tags$meta(charset="utf-8"),
     tags$meta(property="og:site_name", content="US EPA"),
     #tags$link(rel = "stylesheet", type = "text/css", href = "css/uswds.css"),
