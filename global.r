53c28b4699ed523be3ac3fa12bb77364f1894510
Blocksom <Blocksom.Karen@epa.gov>
2024-07-10 10:25:06 -0700

    Updated loading of packages to get rid of loop that automatically installs missing packages. That approach does not work in Posit Connect.

--- global.r
+++ global.r
@@ -1,8 +1,15 @@
-list.of.packages <- c("shiny","spsurvey","Hmisc","shinyjs","shinythemes",'shinybusy','shinyhelper',
-                      'ggplot2', 'ggridges','stringr', 'shinyBS','DT')
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(list.of.packages,function(x){library(x,character.only=TRUE)}) 
+library(shiny)
+library(spsurvey)
+library(Hmisc)
+library(shinyjs)
+library(shinythemes)
+library(shinybusy)
+library(shinyhelper)
+library(ggplot2)
+library(ggridges)
+library(stringr)
+library(shinyBS)
+library(DT)
 
-options(shiny.maxRequestSize = 20*1024^2) #Allows an upload of up to 20mb. 
+# options(shiny.maxRequestSize = 20*1024^2) #Allows an upload of up to 20mb. 
 options(scipen = 100)
